45d2b883054b2d98b58ee19c85f4b520b1827746
Karan Dhanoya <kdhanoya94@gmail.com>
2018-12-26 21:42:58 +0000

    adding ejs

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
+const adminData = require('./admin')
 
 const router = express.Router()
 
 router.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, "../","views", "shop.html"))
+    const products = adminData.products
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: "/"})
 })
 
 module.exports = router
